1b3778c4c6a3e33e50da8efa94925cecc48fda88
Hox117 <arraezgargabriel@gmail.com>
2025-12-16 21:19:39 +0100

    A

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,14 @@ public class Player : MonoBehaviour
     public Rigidbody2D rig;
     private float jumpForce;
     public LayerMask layerMask;
+    private Animator animator;
+
+    private bool esSuelo = false;
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         jumpForce = speed *50;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,15 +24,47 @@ public class Player : MonoBehaviour
         rig.velocity = (transform.right * speed * Input.GetAxis("Horizontal")) + (transform.up * rig.velocity.y);
         if (Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(transform.up * jumpForce);
+            Salta();
         }
         
-        RaycastHit2D suelo = Physics2D.Raycast(transform.position, transform.forward, transform.localScale.y / 2, layerMask);
+        animator.SetFloat("VelocityY", rig.velocity.y);
+        animator.SetFloat("VelocityX", Mathf.Abs(rig.velocity.x));
+        animator.SetBool("SueloToca", TocaSuelo());
+
+        if (rig.velocity.x > 0.1f)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+
+        }
+        else if (rig.velocity.x < -0.1f)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    private bool TocaSuelo()
+    {
+        
+
+        RaycastHit2D suelo = Physics2D.Raycast(transform.position, Vector2.down, transform.localScale.y + 0.1f, layerMask);
+        Debug.DrawRay(transform.position, Vector2.down, Color.red);
         if (suelo.collider != null)
         {
             Debug.Log("Golpeo: " + suelo.collider.name);
+            return  true;
+        } else
+        {
+            return  false;
         }
-        Debug.DrawRay(transform.position, transform.forward * (1f), Color.red);
+            
         
+    }
+
+    private void Salta()
+    {
+        if (TocaSuelo()) { 
+            rig.AddForce(transform.up * jumpForce);
+            
+        }
     }
 }
